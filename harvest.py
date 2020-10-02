e89c96ae7776ed4da040790a732a0357c3a12ec7
Mark Breitenbach <mark.breitenbach@praetorian.com>
2020-10-02 12:39:38 -0400

    Regex-fu on names for better emails.

--- harvest.py
+++ harvest.py
@@ -1,4 +1,9 @@
-import json,urllib2,sys,optparse,argparse
+import json,sys,optparse,argparse,re
+from urllib.request import build_opener
+from unidecode import unidecode
+# macOS brew python hack
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 parser = argparse.ArgumentParser(description='Creates email addresses with optional formatters from names of employees of a company on LinkedIn.')
 parser.add_argument('COMPANY', 
@@ -7,6 +12,10 @@ parser.add_argument('DOMAIN',
                     help='Domain to be used in email address')
 parser.add_argument('COOKIE',
                     help='Cookie file',metavar="FILE")
+
+parser.add_argument("-o", "--output",
+                  default='emails.txt',
+                  help="path to output text file for emails")
 
 parser.add_argument("-f", 
                   action="store_true", dest="abbrevF", default=False,
@@ -19,13 +28,18 @@ parser.add_argument("-l",
 parser.add_argument("-s",
                   action="store_true", dest="swap", default=False,
                   help="switch order of first and last names")
+'''
+#TODO:
+## other possible email transformations:
+### remove/include:
+#### initials ([A-Z]\.)
+#### roman numerals
+#### hyphens
+### additional permutations
+### use nicknames in (.*) 
+'''
 
 args = parser.parse_args()
-
-if(len(sys.argv) < 4):
-	print("Usage: python harvest.py <COMPANY ID> <DOMAIN> <COOKIE FILE>")
-	exit()
-
 
 company_id = args.COMPANY
 domain = args.DOMAIN
@@ -46,79 +60,112 @@ emails = []
 
 last_page = False
 
-def format_name(name):
-	name = name.lower()
-	#remove apostrophes and periods from names
-	name = name.replace('\'','')
-	name = name.replace('.','')
-	name = name.replace(',','')
-	name = name.replace('(','')
-	name = name.replace(')','')
+creds='''AAMS
+ACA
+ADPA
+AIF
+AWMA
+CAIA
+CAP
+CDFA
+CEP
+CFA
+CFP
+ChFC
+CIMA
+CLU
+CPA
+CMA
+CMM
+CMP
+CDFA
+CPWA
+CRPC
+CRPS
+CTP
+CWS
+Jr.
+MBA
+M.A.
+RICP
+Sr.
+WMS'''.split('\n')
 
-	name = name.replace(' ', '.')
-	email = name + '@' + domain + '\n'
-	return email
+def format_name(orig):
+    orig = unidecode(orig)
+    m = re.match(r'\s*(?:(?:(?:[\x80-\xff]|\)(?:[Rr]|[Tt][Mm])\()?(?:%s),?)+|[^,]+,)*\s*([A-Za-z \-\.\'\(\)]+)\s*'%('|'.join([x[::-1] for x in creds])), orig[::-1])
+    if not m:
+        print('unexpected format for %s' % orig)
+    name = m.group(1)[::-1]
+
+    # for now, remove
+    ## paren content
+    ## initials (only if -f, -l not specified)
+    ## all non local email chars
+    final = re.subn(r'(\(.*\)%s|[-\'\.,\\])' % (r'|[A-Z]\.' if (not args.abbrevF and not args.abbrevL) else ''), '', name)[0].lower().split(' ')
+    email = '.'.join([f for f in final if len(f)>0]) + '@' + domain
+    #print('%50s -> %40s -> %50s -> %40s'%(orig,name,final,email))
+    
+    return email
 
 def harvest(curr):
-	global last_page
+    global last_page
 
-	url = ("https://www.linkedin.com/voyager/api/search/blended?"
-	"count=49&origin=OTHER&queryContext=List(spellCorrectionEnabled-%3Etrue,"
-	"crelatedSearchesEnabled-%3Etrue,kcardTypes-%3EPROFILE%7CCOMPANY)&q=all&filters=List(currentCompany-%3E" + company_id + ",resultType-%3EPEOPLE)&start=" + str(curr))
-	opener = urllib2.build_opener()
-	opener.addheaders.append(('csrf-token', csrf_token))
-	opener.addheaders.append(('Cookie', cookie1 + ';' + cookie2))
-	opener.addheaders.append(('x-restli-protocol-version', '2.0.0'))
-	response = opener.open(url)
+    url = ("https://www.linkedin.com/voyager/api/search/blended?"
+    "count=49&origin=OTHER&queryContext=List(spellCorrectionEnabled-%3Etrue,"
+    "crelatedSearchesEnabled-%3Etrue,kcardTypes-%3EPROFILE%7CCOMPANY)&q=all&filters=List(currentCompany-%3E" + company_id + ",resultType-%3EPEOPLE)&start=" + str(curr))
+    opener = build_opener()
+    opener.addheaders.append(('csrf-token', csrf_token))
+    opener.addheaders.append(('Cookie', cookie1 + ';' + cookie2))
+    opener.addheaders.append(('x-restli-protocol-version', '2.0.0'))
+    response = opener.open(url)
 
-	data = json.load(response)
-	data = data["elements"][0]
-	data = data["elements"]
+    data = json.load(response)
+    data = data["elements"][0]
+    data = data["elements"]
 
-	if len(data) != 49:
-		last_page = True
-	
-	for employee in data:
-		employee = employee["image"]
-		employee = employee["attributes"]
-		employee = employee[0]
-		employee = employee["miniProfile"]
-		fname = employee["firstName"]
-		lname = employee["lastName"]
-		fname = fname.replace(' ', '')
-		lname = lname.replace(' ', '')
-		if fname == "":
-			continue
-		if args.abbrevF:
-			fname = fname[0:1]
-		if args.abbrevL:
-			lname = lname[0:1]
-		if args.swap:
-			fullname = lname + ' ' + fname
-		else:
-			fullname = fname + ' ' + lname
+    if len(data) != 49:
+        last_page = True
+    
+    for employee in data:
+        employee = employee["image"]
+        employee = employee["attributes"]
+        employee = employee[0]
+        employee = employee["miniProfile"]
+        fname = employee["firstName"]
+        lname = employee["lastName"]
+        fname = fname.replace(' ', '')
+        lname = lname.replace(' ', '')
+        if fname == "":
+            continue
+        if args.abbrevF:
+            fname = fname[0:1]
+        if args.abbrevL:
+            lname = lname[0:1]
+        if args.swap:
+            fullname = lname + ' ' + fname
+        else:
+            fullname = fname + ' ' + lname
 
-		email = format_name(fullname)
-		emails.append(email)
+        email = format_name(fullname)
+        emails.append(email)
 
 curr = 0
 while not last_page:
-	harvest(curr)
-	curr = curr + 49
+    harvest(curr)
+    curr = curr + 49
 
 
-f = open('./emails.txt',"w+")
+f = open(args.output,"w+")
 count = len(emails)
 for i in emails:
    try:
-      f.write(i)
+      f.write('%s\n'%i)
    except:
-      print('error writing one email due to strange character')
+      print('error writing one email ("%s") due to strange character'%i)
     
 
 f.close()
 
-print("Done! " + str(count) + " emails have been written to ./emails.txt")
+print("Done! " + str(count) + " emails have been written to %s" % args.output)
       
-
-
